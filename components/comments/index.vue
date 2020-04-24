e26517ebe0b66a8ae2c56240b99139200cfb5280
vladkuraev84 <vladkuraev84@gmail.com>
2020-04-24 19:44:18 +0300

    added styles for footer

--- components/comments/index.vue
+++ components/comments/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container">
-    <div class="comments">
+  <div class="comments">
+    <div class="container">
       <div class="comments__header">
         <div class="comments__info">
           <p><b>Последние отзывы</b></p>
@@ -22,24 +22,30 @@
           />
         </ul>
       </div>
-      <hr>
-      <form @submit.prevent="addNewTodo">
-        <label>Добавить задачу</label>
-        <!--<input
-          v-model.trim="newTodoText"
-          placeholder="Например"
-        >-->
-        <div
-          contenteditable
-          ref="box"
-          @input="event => newTodoText = event.target.innerText"
-          @keydown.enter="addNewTodo"
-        />
-        <button>Добавить</button>
-      </form>
+    </div>
+    <div class="comment__form">
+      <div class="container">
+        <form @submit.prevent="addNewTodo">
+          <!--<input
+            v-model.trim="newTodoText"
+            placeholder="Например"
+          >-->
+          <div class="input">
+            <div
+              class="textarea"
+              contenteditable
+              ref="box"
+              @input="event => newTodoText = event.target.innerText"
+              @keydown.enter="addNewTodo"
+            />
+          </div>
+          <div class="text-center">
+            <button class="btn btn--yellow">Написать консультанту</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-
 </template>
 <script>
   import Item from './item'
@@ -53,9 +59,9 @@
       countLike: 131,
       countSms: 14,
       items: [
-        {id: 1, name: "Vlad", comment: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?", dateItem: '13 октября 2011'},
-        {id: 2, name: "Serg", comment: "Lorem ipsum2", dateItem: '14 октября 2011'},
-        {id: 3, name: "Yana", comment: "Lorem ipsum3", dateItem: '1580558031264'}
+        {id: 1, name: "Самуил", comment: "Привет, Верунь! ниче себе ты крутая. фотка класс!!!! ", dateItem: '13 октября 2011'},
+        {id: 2, name: "Лилия Семеновна", comment: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент, это и есть всемирно известный центр огранки алмазов и торговли бриллиантами?", dateItem: '14 октября 2011'},
+        {id: 3, name: "Лилия Семеновна", comment: "Вероника, здравствуйте! Есть такой вопрос: Особый вид куниц жизненно стабилизирует кинетический момент?", dateItem: '14 октября 2011'}
       ],
       nextTodoId: 4
     }),
